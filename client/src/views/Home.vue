5ff06b261fba8636788e1362335be5fc0cde6f35
Ilfat Karimov <60223526+IlfatKar@users.noreply.github.com>
2022-01-30 15:35:01 +0300

    Add files via upload

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,65 +1,70 @@
 <template>
   <div class="home">
-    <div class='online-status'>В сети: {{onlineCount}}</div>
+    <div class="online-status">В сети: {{ onlineCount }}</div>
     <div class="chat" ref="chat">
-      <Message v-for="msg of messages" :sender="msg.sender" :me="msg.sender === user" :key="msg.id" :msg="msg"></Message>
+      <Message
+        v-for="msg of messages"
+        :sender="msg.sender"
+        :me="msg.sender === user"
+        :key="msg.id"
+        :msg="msg"
+      ></Message>
     </div>
     <form class="inputs" @submit.prevent="send">
-      <input type="text" v-model.trim="input"/>
-      <button @click="file" class="file">File</button>
-      <input hidden ref="imageRef" type="file" accept=".jpg, .jpeg, .png" @change="setImage"/>
+      <input type="text" v-model.trim="input" />
+      <button type="button" @click="file" class="file">File</button>
+      <input hidden ref="imageRef" type="file" accept=".jpg, .jpeg, .png" @change="setImage" />
       <button type="submit">Send</button>
     </form>
   </div>
 </template>
 
 <style>
-.inputs{
+.inputs {
   display: flex;
   width: 100%;
   gap: 10px;
 }
-.inputs input{
+.inputs input {
   padding: 5px 5px;
   width: 100%;
 }
 
-.inputs button{
+.inputs button {
   width: 120px;
 }
 
-.online-status{
+.online-status {
   color: #fff;
 }
 
-.chat{
-  max-height:60vh;
+.chat {
+  max-height: 60vh;
   overflow: auto;
   margin-bottom: 10px;
   scrollbar-width: thin;
 }
 ::-webkit-scrollbar {
-    height: 5px;
-    width: 5px;
-    background: #002137;
+  height: 5px;
+  width: 5px;
+  background: #002137;
 }
 
 ::-webkit-scrollbar-thumb {
-    background: #002b75;
-    cursor: pointer;
+  background: #002b75;
+  cursor: pointer;
 }
-
 </style>
 
 <script>
 // @ is an alias to /src
-import Message from '@/components/Message.vue';
-import { v4 as uuidv4 } from 'uuid';
-import SimpleCrypto from "simple-crypto-js"
+import Message from '@/components/Message.vue'
+import { v4 as uuidv4 } from 'uuid'
+import SimpleCrypto from 'simple-crypto-js'
 
 export default {
-  name: "Home",
-  data(){
+  name: 'Home',
+  data() {
     return {
       ws: null,
       messages: {},
@@ -76,11 +81,11 @@ export default {
     Message,
   },
   methods: {
-    file(){
+    file() {
       this.$refs.imageRef.click()
     },
     setImage(e) {
-      if(e.target.files[0]){
+      if (e.target.files[0]) {
         const reader = new FileReader()
         reader.onloadend = () => {
           this.image = reader.result
@@ -88,80 +93,89 @@ export default {
         reader.readAsDataURL(e.target.files[0])
       }
     },
-    send(){
-      this.lastId = uuidv4();
-      const data = this.simpleCrypto.encrypt(JSON.stringify(
-        {
+    send() {
+      this.lastId = uuidv4()
+      const data = this.simpleCrypto.encrypt(
+        JSON.stringify({
           id: this.lastId,
           text: this.input,
           sender: this.user,
           image: this.image,
-        }))
-      this.input && this.ws.send(JSON.stringify({
-        action: 'SEND',
-        data,
-      }))
-      this.input = '';
-      this.image = null;
+        })
+      )
+      this.input &&
+        this.ws.send(
+          JSON.stringify({
+            action: 'SEND',
+            data,
+          })
+        )
+      this.input = ''
+      this.image = null
     },
-    scroll(){
-      const el = this.$refs.chat;
+    scroll() {
+      const el = this.$refs.chat
 
       if (el) {
-        el.scrollTop = el.scrollHeight;
+        el.scrollTop = el.scrollHeight
       }
-    }
+    },
   },
-  updated(){
-    if(this.needScroll){
+  updated() {
+    if (this.needScroll) {
       this.scroll()
       this.needScroll = false
     }
   },
-  mounted(){
+  mounted() {
     this.messages = this.$store.getters.getMessages
     this.user = this.$store.getters.getUser
-    if(!this.user){
+    if (!this.user) {
       this.$store.commit('setUser', uuidv4())
       this.user = this.$store.state.userId
     }
-    this.ws = new WebSocket('ws://localhost:9000');
-    this.ws.onmessage = (data => {
-      const parsed0 = JSON.parse(data.data);
+    this.ws = new WebSocket('ws://crystalline-calm-sleet.glitch.me/')
+    this.ws.onmessage = (data) => {
+      const parsed0 = JSON.parse(data.data)
       const parsed = {
         ...parsed0,
-        data: (parsed0.data && parsed0.type !== 'online') ? this.simpleCrypto.decrypt(parsed0.data) : parsed0.data,
+        data:
+          parsed0.data && parsed0.type !== 'online'
+            ? this.simpleCrypto.decrypt(parsed0.data)
+            : parsed0.data,
       }
 
-      if(parsed.type === 'MESSAGE' && parsed.data.id){
+      if (parsed.type === 'MESSAGE' && parsed.data.id) {
         this.messages = {
           ...this.messages,
           [parsed.data.id]: parsed.data,
-        };
+        }
       }
-      if(parsed.type === 'REQUEST_MESSAGES'){
-        this.ws.send(JSON.stringify({
-          action: 'REQUEST_MESSAGES',
-          data: this.simpleCrypto.encrypt(JSON.stringify({messageList: this.messages})),
-        }))
+      if (parsed.type === 'REQUEST_MESSAGES') {
+        this.ws.send(
+          JSON.stringify({
+            action: 'REQUEST_MESSAGES',
+            data: this.simpleCrypto.encrypt(JSON.stringify({ messageList: this.messages })),
+          })
+        )
       }
-      if(parsed.type === 'MESSAGES'){
+      if (parsed.type === 'MESSAGES') {
         this.messages = parsed.data.messageList
         this.needScroll = true
       }
-      if(parsed.type === 'online'){
+      if (parsed.type === 'online') {
         this.onlineCount = parsed.data
       }
-      if(Object.keys(this.messages).length > 50){
+      if (Object.keys(this.messages).length > 50) {
         const tmp = {}
         const keys = Object.keys(this.messages)
-        for(let i = 1; i < keys.length; i++){
+        for (let i = 1; i < keys.length; i++) {
           tmp[keys[i]] = this.messages[keys[i]]
         }
         this.messages = tmp
       }
       this.$store.commit('setMessages', this.messages)
-    })
+    }
   },
-};
+}
 </script>
